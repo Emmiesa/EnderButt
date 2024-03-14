2740df61fa2222489d4b6d824591f0189b6f35d7
Emmiesa <skynetworkdotnet@gmail.com>
2024-03-14 20:28:05 +0100

    EnderButtListener.java

--- src/main/java/me/lisariesey/enderbutt/EnderButt.java
+++ src/main/java/me/lisariesey/enderbutt/EnderButt.java
@@ -1,17 +1,25 @@
 package me.lisariesey.enderbutt;
 
+import lombok.Getter;
+import me.lisariesey.enderbutt.listener.EnderButtListener;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class EnderButt extends JavaPlugin {
+public class EnderButt extends JavaPlugin {
+
+    @Getter public static EnderButt instance;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        instance = this;
+        saveDefaultConfig();
+        Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("console-startup-message")));
+        getServer().getPluginManager().registerEvents(new EnderButtListener(), this);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("console-shutdown-message")));
     }
 }
