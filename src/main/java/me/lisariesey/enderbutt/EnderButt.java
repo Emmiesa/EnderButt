71bde17c14da8d9039b98a5806385ac045a1a73a
Emmiesa <skynetworkdotnet@gmail.com>
2024-03-14 20:27:17 +0100

    Initial commit

---
+++ src/main/java/me/lisariesey/enderbutt/EnderButt.java
@@ -0,0 +1,17 @@
+package me.lisariesey.enderbutt;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EnderButt extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/me/lisariesey/enderbutt/listener/EnderButtListener.java
@@ -0,0 +1,2 @@
+package me.lisariesey.enderbutt.listener;public class EnderButtListener {
+}
